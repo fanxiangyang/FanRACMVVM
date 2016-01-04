e43326829c9637836094e6005568c74455168f7b
fanxiangyang <fanxiangyang_heda@163.com>
2016-01-04 14:08:04 +0800

    Initial Commit

---
+++ FanRACMVVM/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FanRACMVVM
+//
+//  Created by 向阳凡 on 16/1/4.
+//  Copyright © 2016年 凡向阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FanRACMVVM/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FanRACMVVM
+//
+//  Created by 向阳凡 on 16/1/4.
+//  Copyright © 2016年 凡向阳. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
